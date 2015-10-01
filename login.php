4b584ad62a3485cad42d96966ff7765c68ebcdae
Alexander Ortiz <aorti017@ucr.edu>
2015-10-01 16:47:44 -0400

    began user login/creation functionality

---
+++ login.php
@@ -0,0 +1,7 @@
+<html>
+	<body>
+		<!--- Check if valid username, check if valid password, add to database-->
+		Your username is <?php echo $_POST["username"]; ?><br>
+		Your password is <?php echo $_POST["password"]; ?>
+	</body>
+</html>
