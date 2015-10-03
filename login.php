d03de1d3056a34b9c441104d2e6893f6a48ca4c1
Alexander Ortiz <aorti017@ucr.edu>
2015-10-02 21:45:09 -0400

    added option for the user to register, removed php info page

--- login.php
+++ login.php
@@ -6,12 +6,17 @@
 		set login cookies
 		-->
 		<?php 
-			//replace this with code that retrieves the user's hashed password
-			$dbPassword =  password_hash($_POST['password'], PASSWORD_DEFAULT);	
-			if (password_verify($_POST['password'], $dbPassword)) {
-				echo "You're logged in";
-			} else{
-				echo "You're not logged in";
+			if($_POST['register']){
+				//validate user entered info and add to database
+			} 
+			else{
+				//replace this with code that retrieves the user's hashed password
+				$dbPassword =  password_hash($_POST['password'], PASSWORD_DEFAULT);	
+				if (password_verify($_POST['password'], $dbPassword)) {
+					echo "You're logged in";
+				} else{
+					echo "You're not logged in";
+				}
 			}
 		?> 		
 	</body>
