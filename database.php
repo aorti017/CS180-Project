3f4d96377cbbc429f0bb24f2e341a38ac695f44f
Alexander Ortiz <aorti017@ucr.edu>
2015-10-07 21:44:06 -0400

    moved the database credentials into the .ini file

--- database.php
+++ database.php
@@ -1,7 +1,17 @@
 <?php
 	function executeStatement($statement){
 		try{
-		        $pdo = new PDO('mysql:host=localhost;dbname=CS180', 'root', 'cs180group');
+			$db = parse_ini_file("./.ini");
+			
+			$user = $db['user'];
+			$password = $db['password'];
+			$host = $db['host'];
+			$name = $db['name'];
+			$type = $db['type'];
+
+			$pdoStatement = $type.':host='.$host.';dbname='.$name;
+
+		        $pdo = new PDO($pdoStatement, $user, $password);
 			$query = $pdo->prepare($statement);
                 	$query->execute();
                 	$results = $query->fetchAll();
