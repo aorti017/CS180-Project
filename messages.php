8247c7f263ab811a7cce0089ccd1003eb3c014f1
Alexander Ortiz <aorti017@ucr.edu>
2015-10-11 20:45:27 -0700

    started the development of the messaging system

---
+++ messages.php
@@ -0,0 +1,58 @@
+<?php
+	include './database.php';
+	$sender = $_GET['username'];
+	$receiver = $_GET['recpUser'];
+	$lastTimeStamp = $_GET['time'];
+	while(true){
+		if($lastTimeStamp == null){
+			//make query to db to get all messages between users
+			//then turn that array into json, add timestamp
+			//echo it to ajax 
+			$sqlStatement = "SELECT message FROM Messages WHERE sender='".$sender."' AND receiver='".$receiver."' OR sender='".$receiver."' AND receiver='".$sender."'";
+			$results = executeStatement($sqlStatement);
+			$sqlStatement = "SELECT MAX(timestamp) FROM Messages WHERE sender='".$sender."' AND receiver='".$receiver."' OR sender='".$receiver."' AND receiver='".$sender."'";
+	
+			$tsr = executeStatement($sqlStatement);
+			$time = $tsr[0][0];
+			$ret = array(
+				'message'=>$results[0]["message"],
+				'timestamp'=>$time
+			);
+			$json = json_encode($ret);
+			echo $json;
+			break;
+		} 
+		else{
+			//get the largest time from the db, if its greater than the last saved time
+			//then display all the messages from current saved time to largest time
+			//then update the saved time
+			$sqlStatement = "SELECT MAX(timestamp) FROM Messages WHERE sender='".$sender.
+				"' AND receiver='".$receiver."' OR sender='".$receiver."' AND receiver='".$sender."'";
+			$results = executeStatement($sqlStatement);
+			$newTimeStamp = $results[0][0];
+			if($newTimeStamp > $lastTimeStamp){
+				//get new message(s) and return
+				$sqlStatement = "SELECT message FROM Messages WHERE timestamp=".$newTimeStamp.
+					" AND ((sender='".$sender."' AND receiver='".$receiver."') OR (sender='".$receiver.
+					"' AND receiver='".$sender."'))";
+				$results = executeStatement($sqlStatement);
+				$ret = array(
+					'message'=>$results[0]["message"],	
+					'timestamp'=>$newTimeStamp
+				);
+				$json = json_encode($ret);
+				echo $json;
+			}
+			else{
+				$ret = array(
+					'message'=>"",	
+					'timestamp'=>$lastTimeStamp
+				);
+				$json = json_encode($ret);
+				echo $json;
+				sleep(1);	
+			}
+			break;
+		}
+	}
+?>
