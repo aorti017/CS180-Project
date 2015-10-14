6bd1fcc5716fe218455fc7b9adbdb9afeabfa7e7
Yash Kamothi <ykamo001@ucr.edu>
2015-10-14 15:39:12 -0700

    updated sending capability to database

---
+++ sendmessage.php
@@ -0,0 +1,11 @@
+<?php
+	include './database.php';
+	$sender = $_POST['username'];
+	$reciever = $_POST['recpUser'];
+	$newTimeStamp = $_POST['time'];
+	$message = $_POST['message'];
+
+	$toInsert =	"INSERT INTO Messages VALUE (".$sender.",".$reciever.",".$newTimeStamp.",".$message.")";
+	executeStatement($toInsert);
+	echo"success";
+?>
