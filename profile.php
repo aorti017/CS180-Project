9a0a3d17afbfab75769a9d674ab5d04bc3b38feb
Yuting <yfang004@ucr.edu>
2015-10-16 15:05:40 -0700

    add a message function

--- profile.php
+++ profile.php
@@ -38,6 +38,8 @@
 			this.type = type;
 			this.message = message;
 			this.time = time;
+			this.getType = functioncs{ return this.type; };
+			this.getMessage = functioncs{ return this.message; };
 		}	
 		function getNewMessages(t){
 				//somehow get the user name from the session variable
