81e1047125def0c6cf0bd6a7b9459578666cbb47
Alexander Ortiz <aorti017@ucr.edu>
2015-10-21 18:33:43 -0400

    Home page is now the contacts page, crated new Contacts DB table, getContacts.php retrieves contacts for suer and is currently displayed on the console in redirect.php

---
+++ getContacts.php
@@ -0,0 +1,19 @@
+<?php
+    include './database.php';
+    //gets the username for the logged in user
+    $username = $_GET['username'];
+    //sql statement to retrieve all of the logged in user's contacts
+    $statement = "SELECT contact FROM Contacts WHERE username='".$username."'";
+    //returns all of the logged in user's contacts
+    $results = executeStatement($statement);
+    //get elemtns returned from sql and turn into array
+    $contactArray = array();
+    foreach($results as $row){
+        array_push($contactArray, $row["contact"]);
+    }
+    //convert results into json to be returned to calling ajax
+    $ret = array('contacts'=>$contactArray);
+    $json = json_encode($ret);
+    //echo json in order to return it
+    echo $json;
+?>
--- redirect.php
+++ redirect.php
@@ -5,22 +5,43 @@ session_start()
 	<head>
 		<meta charset="UTF-8">
 		<script type="text/javascript" src="http://code.jquery.com/jquery.min.js"></script>
+        <script src="javascript.js"></script>
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 		<title>Home</title>
 		<link rel="stylesheet" type="text/css" href="navigation.css">
 	</head>
-	
+
 	<body>
 		<div id="navigation">
         	<ul>
             	<li><a href="logout.php">Logout</a></li>
-				<li id="currentpage"><a href="">Home</a></li>
+				<li id="currentpage"><a href="">Contatcs</a></li>
 				<li><a href="profile.php">Messages</a></li>
 			</ul>
 		</div>
 		<br>
-		<p>Welcome to WhatsApp</p>
+		<script type="text/javascript">
+            function getContacts(){
+                var username = parse();
+                $.ajax({
+                    type: 'GET',
+                    url: './getContacts.php',
+                    data: {username: username},
+                    success: function(data) {
+                        var obj = jQuery.parseJSON(data);
+                        var conts = obj.contacts;
+                        for(i = 0; i < conts.length; i++){
+                            console.log(conts[i]);
+                        }
+                    }
+                });
+            }
+
+            $(function(){
+                getContacts();
+            });
+        </script>
 	</body>
 </html>
