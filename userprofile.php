e72d10bbdca83747df9be06379f1941d6a29ea9b
Alexander Ortiz <aorti017@ucr.edu>
2015-10-30 19:25:28 -0400

    empty

--- userprofile.php
+++ userprofile.php
@@ -2,10 +2,10 @@
 
 include "./database.php";
 //$user = $_GET['user'];
-$user = "nikita";
+$user = "tom";
 $sql = "SELECT * FROM Users WHERE username = '".$user."'";
 $results = executeStatement($sql);
-$username = $results[0][0];
+$username = $results[0];
 echo $username;
 ?>
 hello
