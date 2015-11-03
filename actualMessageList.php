11355c2942f3582655d413232f9254e0d60ad00e
yli082 <yli082@ucr.edu>
2015-11-02 18:46:53 -0800

    added the message list

--- actualMessageList.php
+++ actualMessageList.php
@@ -1,46 +1,59 @@
 <html>
-<head>
-
-
-<meta charset="UTF-8">
-<title>MessageList</title>
-
-
-
-
-</head>
+	<head>
+		<meta charset="UTF-8">
+		<title>MessageList</title>
+		<script src="javascript.js"></script>
+		<script type="text/javascript" src="http://code.jquery.com/jquery.min.js"></script>
+	</head>
 
 <body>
 
-<script type="text/javascript">
+<script>
+	function getMessageList(){
 
-$var username = "ass";
-function getMessageList(){
-$.ajax({
-	type: 'GET',
-	url: './messageList.php',
-	data: {username: username},
-	success: function(data){
-		var obj = jQuery.parseJSON(data);
-		var senders = obj.sender;
-		var receivers = obj.receiver;
-		var timestamps = obj.timestamp;
+		var username = parse();
+		$.ajax({
+			type: 'GET',
+			url: './messageList.php',
+			data: {username: username},
+			success: function(data){
+				var obj = jQuery.parseJSON(data);
+				var senders = obj.sender;
+				var receivers = obj.receiver;
+				var timestamps = obj.timestamp;
+				for(var i = 0; i < senders.length; i++)
+				{
+					if (senders[i] == username) {
+						var btn = document.createElement("BUTTON");
+						btn.setAttribute("id", "Btn");
+						btn.setAttribute("value", receivers[i]);
+						btn.onclick=function(){
+							window.location.replace("./messages.php?contact=" + this.value);
+						};
+						var t = document.createTextNode(receivers[i]);
+						btn.appendChild(t);
+						document.body.appendChild(btn);
+					}
+					else {
+						var btn = document.createElement("BUTTON");
+						btn.setAttribute("id", "Btn");
+						btn.setAttribute("value", senders[i]);
+						btn.onclick=function(){
 
-		for(i = 0; i < senders.length;++i)
-		{
-			console.log(senders[i]);
-		}
-
-
-
-}
-});
-}
-$(function(){
-	getMessageList();
-});
-
-
+							window.location.replace("./messages.php?contact=" + this.value);
+						};
+						var t = document.createTextNode(senders[i]);
+						btn.appendChild(t);
+						document.body.appendChild(btn);
+					}
+					
+				}
+			}
+		});
+	}
+	$(function(){
+		getMessageList();
+	});
 
 </script>
 
