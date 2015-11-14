f49c0fb9a4ff5015819a461f98564294e5272219
Yash Kamothi <ykamo001@ucr.edu>
2015-11-13 16:24:40 -0800

    updated login page

--- index.php
+++ index.php
@@ -8,16 +8,23 @@
 ?>
 
 <html>
+	<link href='logindesign.css' rel='stylesheet' type='text/css'>
 	<head>
 		<title>CS180 Project</title>
 	</head>
+	</<div class="logo"></</div>
+	<div class="login-block">
+	    <h1>Login</h1>
         <form action="login.php" method="post">
-			Username: <input type="username" name="username" placeholder="John Doe"><br>
-			Password: <input type="password" name="password"><br>
-			<input type="submit" value="Login" name="login">
+			<input type="username" name="username" placeholder="Username" id="username"><br>
+			<input type="password" name="password" placeholder="Password" id="password"><br>
+			<button>Submit</button>
+			</<input type="submit" value="Login" name="login"> 
 		</form>
 	    <form action="register.php" method="post">
-    		<input type="submit" value="Register">
+    		<button>Register</button>
+    		</<input type="submit" value="Register">
         </form>
+    </div>
 	</body>
 </html>
