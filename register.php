83e73d23ee6bd44a9fa1f0a1449eabc7309a792f
Yash Kamothi <ykamo001@ucr.edu>
2015-11-13 17:09:39 -0800

    updated register page

--- register.php
+++ register.php
@@ -13,29 +13,28 @@
 	<head>
 		<title>Register Profile</title>
 	</head>
+	<link href='registerdesign.css' rel='stylesheet' type='text/css'>
 	<body>
-    	<form action="makeUser.php" method="POST">
-				Username*: <br>
-                <input type="text" name="username" id="username" placeholder="jdoe001"><br><br>
-				Password*: <br>
-                <input type="password" name="password" id="password"><br><br>
-				First Name: <br>
-                <input type="text" name="firstname" id="firstname" placeholder="John"><br><br>
-                Last Name: <br>
-				<input type="text" name="lastname" id="lastname" placeholder="Doe"><br><br>
-				Birthday: <br>
-                <input type="date" name="birthdate" id="birthdate" placeholder="yyyy/mm/dd"><br><br>
-				Email: <br>
-                <input type="text" name="email" id="email" placeholder="Obama@whitehouse.gov"><br><br>
-				Gender: <br>
-                <input type="radio" name="gender" value="Male" >Male
+		<div class="wrapper" align="center">
+			<h1>Register For An Account</h1>
+			<p>To sign-up for a free basic account please provide us with some basic information using
+			the contact form below. Please use valid credentials.</p>
+			<form action="makeUser.php" method="POST">
+				<input type="text" name="username" id="username" placeholder="Username"><br><br>
+				<input type="password" name="password" id="password" placeholder="Password"><br><br>
+				<input type="text" name="firstname" id="firstname" placeholder="First Name"><br><br>
+				<input type="text" name="lastname" id="lastname" placeholder="Last Name"><br><br>
+				<input type="date" name="birthdate" id="birthdate" placeholder="yyyy/mm/dd"><br><br>
+				<input type="email" name="email" id="email" placeholder="Email"><br><br>
+				<input type="radio" name="gender" value="Male" >Male
 				<input type="radio" name="gender" value="Female">Female
 				<br>
+				<br>
 				<input type="submit" value="Create Account">
-		</form>
-
-        <form action="index.php" method="POST">
-    		<input type="submit" value="Cancel">
-        </form>
+			</form>
+			<form action="index.php" method="POST">
+				<input type="submit" value="Cancel">
+			</form>
+		</div>
 	</body>
 </html>
